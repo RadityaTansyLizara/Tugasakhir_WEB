a638cbcb3f2028eb400001fd342a74ea16301c41
RadityaTansyLizara <147571863+RadityaTansyLizara@users.noreply.github.com>
2025-06-30 11:21:42 +0700

    Delete app directory

--- app/Views/article/detail.php
+++
@@ -1,9 +0,0 @@
-<?= $this->include("template/header") ?>
-<article class="entry">
-<h2><?= $article["title"] ?></h2>
-<img src="<?= base_url("/image/" . $article["image"]) ?>" alt="<?= $article[
-    "slug"
-] ?>">
-<p><?= $article["content"] ?></p>
-</article>
-<?= $this->include("template/footer") ?>
